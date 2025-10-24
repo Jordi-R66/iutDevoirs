c8a50b71be318e86ebd808252abb330fba26d590
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-10-24 19:04:15 +0200

    Ajout des inclusions

--- src/server/common.h
+++ src/server/common.h
@@ -2,7 +2,12 @@
 
 #ifndef SERVER_COMMON
 
+#include "../common/includes.h"
+#include "../common/datatypes.h"
+#include "../common/endianness.h"
+#include "../common/config.h"
 
+#include <pthread.h>
 
 #define SERVER_COMMON
 #endif
