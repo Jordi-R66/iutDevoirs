14ca916e4b335fcb9e0f19172bb26b4d080241c4
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-10-24 09:08:39 +0200

    Implémentation de la fonction de création d'une struct config

--- src/common/config.c
+++ src/common/config.c
@@ -4,4 +4,13 @@
 
 #ifdef COMMON_CONFIG
 
+Config createConfig(uint8_t ip[4], uint16_t port) {
+	detectEndian();
+	Config output = {sysEndianness, 0, port};
+
+	output.ip = *(uint32_t*)ip;
+
+	return output;
+}
+
 #endif
