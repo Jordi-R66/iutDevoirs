46561e3dfe9b95bbbc9e8eae9b15eb8519566e80
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-10-24 08:59:34 +0200

    Ajout du header pour le fichier de config réseau

---
+++ src/common/config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef COMMON_CONFIG
+
+#include "includes.h"
+
+#pragma pack(1)
+typedef struct sockaddr_in sockAddrIn;
+
+typedef struct Config {
+	Endianness_t endianness;
+	uint32_t ip;
+	uint16_t port;
+} Config;
+
+#define CONFIG_SIZE sizeof(Config)
+
+#pragma pack()
+
+Config createConfig(uint8_t ip[4], uint16_t port);
+Config readConfig(char* filename);
+void writeConfig(char* filename, Config config);
+
+sockAddrIn configToSockAddr(Config config);
+
+#define COMMON_CONFIG
+#endif
