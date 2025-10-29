46d7de3a398db6f5e69ab9fd4cd46c2e549951a0
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-10-29 13:33:34 +0100

    Implémentation de la fonction d'initialisation de la queue

--- src/threading/queue.c
+++ src/threading/queue.c
@@ -1,1 +1,11 @@
 #include "queue.h"
+
+void initQueue(QPtr queue) {
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->stop = false;
+
+	pthread_mutex_init(&queue->mutex, NULL);
+	pthread_cond_init(&queue->cond, NULL);
+}
+
