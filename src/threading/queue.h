fa4040b7e65345c9015137defba37497b4bbfb5d
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-10-29 12:52:37 +0100

    Ajout de la déclaration de fonctions pour les Queues

--- src/threading/queue.h
+++ src/threading/queue.h
@@ -11,18 +11,27 @@ typedef struct queue_node {
 	QNodePtr next;
 } QNode, *QNodePtr;
 
-typedef struct {
+typedef struct queue {
 	QNodePtr head, tail;
 
 	pthread_mutex_t mutex;
 	pthread_cond_t cond;
 	bool stop;
-} Queue;
+} Queue, *QPtr;
 
 #pragma pack()
 
 #define QNODE_SIZE sizeof(QNode)
 #define QUEUE_SIZE sizeof(Queue)
+
+// Fonctions
+
+void initQueue(QPtr Queue);
+void destroyQueue(QPtr Queue);
+
+void pushQueue(QPtr Queue, ptr data);
+ptr popQueue(QPtr Queue);
+void stopQueue(QPtr Queue);
 
 #define QUEUE_HEADER
 #endif
