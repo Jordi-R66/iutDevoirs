a7b7f2617e1902dc2191cd387bbf240a5afdd3ac
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-11-07 09:31:01 +0100

    Ajout de code de test

--- src/server/server.c
+++ src/server/server.c
@@ -1,5 +1,12 @@
-#include "../common/datatypes.h"
+#include "common.h"
 
-int main(void) {
-	
+
+int main(int argc, char** argv) {
+	detectEndian();
+
+	Config config = parseConfig("246.176.156.57", "5754");
+
+	printf("It's all damn right! PORT : %hu\n", config.port);
+
+	return 0;
 }
