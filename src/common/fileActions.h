8cf1cfa068a0cda725960b5b46cc017f3bf27d58
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-11-14 08:47:15 +0100

    Ajout d'un enums pour associer des codes aux actions possibles

--- src/common/fileActions.h
+++ src/common/fileActions.h
@@ -2,7 +2,38 @@
 
 #ifndef FILE_ACTIONS
 
+typedef enum Actions {
+	UNKNOWN = 0,
+	// ---------------------------
+	ADD_USER = 1,
+	READ_USER = 2,
+	EDIT_USER = 3,
+	DEL_USER = 4,
+	// ---------------------------
+	ADD_HOMEWORK = 5,
+	READ_HOMEWORK = 6,
+	EDIT_HOMEWORK = 7,
+	DEL_HOMEWORK = 8
+	// ---------------------------
+} Actions;
 
+typedef struct {
+	ptr response;
+	size_t responseSize;
+
+	bool hasReturned;
+} ActionResponse, *ActionRespPtr;
+
+typedef struct {
+	Actions actionType;
+	size_t actionSize;
+	ptr actionBody;
+
+	ActionResponse response;
+} ActionRequest, *ActionReqPtr;
+
+#define ACTION_REQ_SIZE sizeof(ActionRequest)
+#define ACTION_RESP_SIZE sizeof(ActionResponse)
 
 #define FILE_ACTIONS
 #endif
